c77648e450eae6017872b052d5ff0934cdd20400
usercypher <lloydbersabe@gmail.com>
2025-02-14 19:20:17 +0800

    .

--- core/config/ini.dev.php
+++ core/config/ini.dev.php
@@ -16,16 +16,6 @@ return array(
     'log_errors' => 1, // Log errors
     'error_log' => 'error_log', // Log file path
 
-    // Session Settings
-    'session.use_only_cookies' => true, // Use cookies for sessions
-    'session.use_cookies' => 'On', // Ensure cookies for sessions
-    'session.use_trans_sid' => 'Off', // No session IDs in URL
-    'session.cookie_httponly' => 'On', // Protect cookies from JS
-    'session.cookie_lifetime' => 90, // Cookie lifetime
-    'session.gc_maxlifetime' => 3600, // Max session lifetime
-    'session.gc_probability' => 1, // GC probability
-    'session.gc_divisor' => 100, // GC divisor
-
     // General Settings
     'default_charset' => 'UTF-8', // Charset UTF-8
 
--- core/config/ini.prod.php
+++ core/config/ini.prod.php
@@ -15,18 +15,6 @@ return array(
     'log_errors' => 1, // Log errors
     'error_log' => 'error_log', // Log file path
 
-    // Session Settings
-    'session.use_only_cookies' => true, // Use cookies for sessions
-    'session.use_cookies' => 'On', // Ensure cookies for sessions
-    'session.use_trans_sid' => 'Off', // No session IDs in URL
-    'session.cookie_httponly' => 'On', // Protect cookies from JS
-    'session.cookie_lifetime' => 3600, // Cookie lifetime for production
-    'session.gc_maxlifetime' => 3600, // Max session lifetime
-    'session.gc_probability' => 1, // GC probability
-    'session.gc_divisor' => 100, // GC divisor
-    'session.cookie_secure' => 'On', // Secure cookies (production)
-    'session.cookie_samesite' => 'Strict', // CSRF protection (production)
-
     // General Settings
     'default_charset' => 'UTF-8', // Charset UTF-8
 
