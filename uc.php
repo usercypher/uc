84297423a38b5ac48105c9e189662a555c7a227d
usercypher <lloydbersabe@gmail.com>
2025-05-19 01:12:26 +0800

    .

--- uc.php
+++ uc.php
@@ -1,19 +1,19 @@
 <?php
-/**
- * Copyright 2025 Lloyd Miles M. Bersabe
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/*
+Copyright 2025 Lloyd Miles M. Bersabe
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 init();
 
@@ -134,7 +134,7 @@ class Response {
     function json($data) {
         $this->type = 'application/json';
         $this->content = json_encode($data);
-        if (json_last_error() !== JSON_ERROR_NONE) $this->content = '{"error": "' . json_last_error_msg() . '"}';
+        if ($this->error = json_last_error() !== JSON_ERROR_NONE) $this->content = '{"error": "' . json_last_error_msg() . '"}';
 
         return $this;
     }
