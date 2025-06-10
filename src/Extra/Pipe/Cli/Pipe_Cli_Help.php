79fe8a18f853cef5a26e1ed52fe70dcf65d8d453
usercypher <lloydbersabe@gmail.com>
2025-06-10 13:43:48 +0800

    .

--- src/Extra/Pipe/Cli/Pipe_Cli_Help.php
+++ src/Extra/Pipe/Cli/Pipe_Cli_Help.php
@@ -12,18 +12,37 @@ class Pipe_Cli_Help {
     public function pipe($request, $response) {
         $break = false;
 
-        $output = 'No route \'' . trim(urldecode(str_replace('/', ' ', $request->path))) . '\' found, list:'. EOL;
+        $output = '';
 
         $routes = $this->flattenRoutesWithMethod($this->app->routes);
 
         sort($routes);
 
+        $target = isset($request->cli['option']['autocomplete']) ? $request->cli['option']['autocomplete'] : null;
+
         $seen = array();
-        foreach ($routes as $route) {
-            $pathParts = explode('/', $route['path']);
-            if (isset($seen[$pathParts[0]]) || substr($pathParts[0], 0, 1) === '{') continue;
-            $seen[$pathParts[0]] = true;
-            if ($route['method'] === '') $output .= ' Route \'' . str_replace('/', ' ', $pathParts[0]) . '\'' . EOL;
+        if (!$target || $target === true) {
+            $output .= 'No route \'' . trim(urldecode(str_replace('/', ' ', $request->path))) . '\' found, list:'. EOL;
+            foreach ($routes as $route) {
+                $pathParts = explode('/', $route['path']);
+                if (isset($seen[$pathParts[0]]) || substr($pathParts[0], 0, 1) === '{') continue;
+                $seen[$pathParts[0]] = true;
+                if ($route['method'] === '') $output .= ' Route \'' . str_replace('/', ' ', $pathParts[0]) . '\'' . EOL;
+            }
+       } else {
+            $matched = false;
+            foreach ($routes as $route) {
+                $pathParts = explode('/', $route['path']);
+                if ($pathParts[0] === $target && isset($pathParts[1])) {
+                    if (isset($seen[$pathParts[1]]) || substr($pathParts[1], 0, 1) === '{') continue;
+                    $seen[$pathParts[1]] = true;
+                    $output .= ' ' . $pathParts[1] . EOL;
+                    $matched = true;
+                }
+            }
+            if (!$matched) {
+                $output .= 'No sub-route found for \'' . $target . '\'' . EOL;
+            }
         }
 
         $response->std($output, true);
