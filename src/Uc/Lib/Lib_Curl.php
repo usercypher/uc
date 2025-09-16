72cac9cbb12cab6808ef9f15bbd0e8e986f4b9eb
usercypher <lloydbersabe@gmail.com>
2025-09-16 17:42:05 +0800

    .

--- src/Uc/Lib/Lib_Curl.php
+++ src/Uc/Lib/Lib_Curl.php
@@ -1,9 +1,11 @@
 <?php
 
 class Lib_Curl {
-    var $result = null;
+    var $result = null, $headersRaw = array();
 
     function send($url, $options = array()) {
+        $this->result = new stdClass();
+        $this->headersRaw = array();
         $ch = curl_init();
 
         $method  = isset($options['method']) ? strtoupper($options['method']) : 'GET';
@@ -13,6 +15,7 @@ class Lib_Curl {
 
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
 
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
@@ -21,15 +24,24 @@ class Lib_Curl {
             curl_setopt($ch, CURLOPT_POSTFIELDS, $content);
         }
 
+        $verify_ssl = isset($options['verify_ssl']) ? $options['verify_ssl'] : true;
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, $verify_ssl);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, $verify_ssl ? 2 : 0);
+
+        if (isset($options['ca_info'])) {
+            curl_setopt($ch, CURLOPT_CAINFO, $options['ca_info']);
+        }
+
         if (!empty($headers)) {
             $parsedHeaders = array();
             foreach ($headers as $key => $value) $parsedHeaders[] = $key . ':' . $value;
             curl_setopt($ch, CURLOPT_HTTPHEADER, $parsedHeaders);
         }
 
-        curl_setopt($ch, CURLOPT_HEADERFUNCTION, array($this, 'header'));
+        $that =& $this;
 
-        $this->result = new stdClass();
+        curl_setopt($ch, CURLOPT_HEADERFUNCTION, array($that, 'header'));
+
         $this->result->headers = array();
         $this->result->content = curl_exec($ch);
         $this->result->code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
@@ -42,11 +54,37 @@ class Lib_Curl {
         return $this->result;
     }
 
-    function header($ch, $header) {
-        if (strpos($header, ':') !== false) {
-            list($key, $value) = explode(':', $header, 2);
-            $this->result->headers[strtolower(trim($key))] = trim($value);
+    function header($ch, $headerLine) {
+        $trimmed = trim($headerLine);
+
+        if ($trimmed === '') {
+            $this->result->headers = $this->parseHeaders($this->headersRaw);
+            $this->headersRaw = array();
+        } else {
+            $this->headersRaw[] = $headerLine;
         }
-        return strlen($header);
+
+        return strlen($headerLine);
+    }
+
+    function parseHeaders($headerLines) {
+        $headers = array();
+        foreach ($headerLines as $line) {
+            if (strpos($line, ':') !== false) {
+                list($key, $value) = explode(':', $line, 2);
+                $key = strtolower(trim($key));
+                $value = trim($value);
+                if (!isset($headers[$key])) {
+                    $headers[$key] = $value;
+                } else {
+                    if (is_array($headers[$key])) {
+                        $headers[$key][] = $value;
+                    } else {
+                        $headers[$key] = array($headers[$key], $value);
+                    }
+                }
+            }
+        }
+        return $headers;
     }
 }
