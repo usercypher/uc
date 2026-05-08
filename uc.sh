73880a3e79369c7afb7fbb43afc20180ca52d5d4
usercypher <lloydbersabe@gmail.com>
2026-05-09 06:13:04 +0800

    .

--- uc.sh
+++ uc.sh
@@ -15,6 +15,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+help() {
+    printf "Usage: %s [-l lighttpd-bin] [-p php-fpm-bin]\n" "$0" >&2
+    exit 1
+}
+
+while [ $# -gt 0 ]; do
+    case $1 in
+        -l) [ $# -ge 2 ] || help
+            LIGHTTPD_BIN=$2
+            shift
+            ;;
+        -p)
+            [ $# -ge 2 ] || help
+            PHP_FPM_BIN=$2
+            shift
+            ;;
+        *) help ;;
+    esac
+    shift
+done
+
 RUNNING=1
 ROOT=$(cd "$(dirname "$0")" && pwd) || exit 1
 UID=$(id -u)
@@ -38,27 +59,13 @@ substitute "$ROOT/uc.sh.lighttpd.conf" "$ROOT/var/dat/lighttpd.conf"
 substitute "$ROOT/uc.sh.php-fpm.conf" "$ROOT/var/dat/php-fpm.conf"
 substitute "$ROOT/uc.sh.php.ini" "$ROOT/var/dat/php.ini"
 
-help() {
-    printf "Usage: %s [-l lighttpd_path] [-p php-fpm_path]\n" "$0" >&2
-    exit 1
-}
-
-while [ $# -gt 0 ]; do
-    case $1 in
-        -l) [ $# -ge 2 ] || help; LIGHTTPD_BIN=$2; shift ;;
-        -p) [ $# -ge 2 ] || help; PHP_FPM_BIN=$2; shift ;;
-        *)  help ;;
-    esac
-    shift
-done
-
 while [ "$RUNNING" -eq 1 ]; do
-    [ -n "${FPM_PID:-}" ] && kill -0 "$FPM_PID" 2>/dev/null || {
+    { [ -n "${FPM_PID:-}" ] && kill -0 "$FPM_PID" 2>/dev/null; } || {
         "${PHP_FPM_BIN:-php-fpm}" -y "$ROOT/var/dat/php-fpm.conf" -c "$ROOT/var/dat/php.ini" --nodaemonize &
         FPM_PID=$!
     }
 
-    [ -n "${LIGHTTPD_PID:-}" ] && kill -0 "$LIGHTTPD_PID" 2>/dev/null || {
+    { [ -n "${LIGHTTPD_PID:-}" ] && kill -0 "$LIGHTTPD_PID" 2>/dev/null; } || {
         "${LIGHTTPD_BIN:-lighttpd}" -D -f "$ROOT/var/dat/lighttpd.conf" &
         LIGHTTPD_PID=$!
     }
@@ -70,7 +77,7 @@ for pid in ${FPM_PID:-} ${LIGHTTPD_PID:-}; do
     kill "$pid" 2>/dev/null
 done
 
-for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30; do
+for _ in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30; do
     alive=
     for pid in ${FPM_PID:-} ${LIGHTTPD_PID:-}; do
         kill -0 "$pid" 2>/dev/null && alive=1 && break
